96fd5fb6fd1f69922591012683d0d4def951a1ae
KirbiKaka <calvinlu@berkeley.edu>
2014-09-29 15:12:37 -0700

    GODPLS

---
+++ BATTLE/Assets/TurnStateMachine.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnStateMachine : MonoBehaviour {
+	//Probably just true, like, all the time
+	bool battle = true;
+	/**
+	 * Who's turn it is.
+	 * 0 = Player's turn
+	 * 1 = First enemy's turn
+	 * 2 = Second enemy's turn
+	 * ...
+	 * n = n-th enemy's turn
+	 **/
+	public static int whosTurn = 0;
+	public static int numEnemies = 1;
+	/**
+	 * What action is currently selected. e.g. this changes after you press the attack button and you're choosing a target.
+	 * 0 = No selection
+	 * 1 = attack
+	 **/
+	public static int commandSelection = 0;
+	
+	public static int playerHP = 100;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		while (battle) {
+			if (whosTurn == 0) {
+				if (Input.GetButtonDown ("Fire1")) {
+					TurnStateMachine.nextTurn ();
+					Debug.Log ("asdkfsdf");
+				}
+			}
+			//player turn
+			//enemy turn
+		}
+		
+	}
+	
+	public static int getTurn() { return whosTurn; }
+	
+	public static void nextTurn() {
+		whosTurn++;
+		if (whosTurn > numEnemies)
+			whosTurn = 0;
+	}
+}
+
