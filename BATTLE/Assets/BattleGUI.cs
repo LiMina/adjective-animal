f0ee24318085a86e11363f4b4e0ca7b6c30cd406
Tracy <lunartofu@gmail.com>
2014-09-30 15:44:53 -0700

    added some battle GUI boxes and a button

--- BATTLE/Assets/BattleGUI.cs
+++ BATTLE/Assets/BattleGUI.cs
@@ -2,14 +2,31 @@
 using System.Collections;
 
 public class BattleGUI : MonoBehaviour {
-
-	//toggle
+	
 	public bool attackLock = false;
-
-	// GUI yay!!
+	private string playerTurnString = "Please select a move.";
+	
 	void OnGUI() {
-	//Announcer
-		GUI.Box (new Rect (25, 25, Screen.width - 50, 50), "This is the announcer.");
+	/*Announcer
+	 * This section will display what attack the enemy used or playerTurnString.
+	 */
+	
+	if (!attackLock)
+		GUI.Box (new Rect (25, 25, Screen.width - 50, 50), playerTurnString);
+
+	//HP/MP display
+		GUI.Box (new Rect (50, Screen.height - 200, 300, 150), ""); 
+
+	//Attack Menu
+		GUI.Box (new Rect (375, Screen.height - 200, 300, 150), "");
+		if (TurnStateMachine.whosTurn == 0) {
+			attackLock = false;
+		} 
+		else {	
+			attackLock = true;
+		}
+		GUI.enabled = !attackLock;
+		GUI.Button(new Rect(380, Screen.height - 180, 295, 25), "Tackle");
 
 	}
 }
