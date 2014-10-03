b42991683bcb00c3f99aa1e94d8ca821e1963d24
austinshyu <austinshyu@berkeley.edu>
2014-10-02 19:57:26 -0700

    hi hunkiedorie it's all fine woooooooo yeah im a comment wow this is so cool I love life it is fantastic someone save me oh no

--- austin-scripts/textmanage.cs
+++ austin-scripts/textmanage.cs
@@ -10,24 +10,32 @@ public class textmanage : MonoBehaviour
 		public string scene = "room";
 		public string dialogue = "WAKE UP!!!!!";
 		public int fontSize = 18;
+	public int classesTaken = 0;
+	public bool lunch = false;
 		// Use this for initialization
 		void Start ()
 		{
 	
 		}
-	
+
+		void Reset_statepath ()
+		{
+				state = 0;
+				path = 0;
+		}
 		// Update is called once per frame
 		void Update ()
 		{
 				if (Input.GetMouseButtonDown (0) && !choosingOption) {
 
 						if (scene == "room" && state == 0 && path == 2) {
-								state = 0;
-								path = 0;
+								Reset_statepath ();
 						} else if (scene == "room" && state == 0 && path == 1) {
 								scene = "breakfast";
-								state = 0;
-								path = 0;
+								Reset_statepath ();
+						} else if (scene == "breakfast" && state == 0 && path != 0) {
+								scene = "school";
+								Reset_statepath ();
 						} else {
 								state++;
 						}
@@ -42,13 +50,25 @@ public class textmanage : MonoBehaviour
 						clickedbutton = 0;
 						path = 2;
 						state = 0;
+				} else if (clickedbutton == 3) {
+						choosingOption = false;
+						clickedbutton = 0;
+						path = 3;
+						state = 0;
+				} else if (clickedbutton == 4) {
+						choosingOption = false;
+						clickedbutton = 0;
+						path = 4;
+						state = 0;
 				} 
 		}
 		//void onMouseClick
 		void OnGUI ()
 		{
-				GUI.Box (new Rect (80, Screen.height - 120, Screen.width - 160, 100), dialogue);
+			
+				GUI.Box (new Rect (80, Screen.height - 270, Screen.width - 160, 250), dialogue);
 				GUI.skin.box.fontSize = fontSize;	
+				GUI.skin.box.wordWrap = true;
 
 				/*
 				 *ROOOOOMMM SCEEEENNNNEEEE
@@ -57,7 +77,7 @@ public class textmanage : MonoBehaviour
 						if (state == 0 && path == 0) {
 								dialogue = "WAKE UP!!!!!";
 						} else if (state == 1 && path == 0) {
-								dialogue = "You roll over, groan and hit your alarm clock\n";		
+								dialogue = "You roll over, groan and hit your alarm clock";		
 						} else if (state == 2 && path == 0) {
 								choosingOption = true;
 								dialogue = "What will you do?";
@@ -71,7 +91,7 @@ public class textmanage : MonoBehaviour
 								dialogue = "Really?";
 						
 						} else if (state == 0 && path == 1) { // option 1
-								dialogue = "Sighing, you push yourself out of bed and\n stand shakily while you blink blearily. ";
+								dialogue = "Sighing, you push yourself out of bed and stand shakily while you blink blearily. ";
 						}
 							
 				}
@@ -80,17 +100,37 @@ public class textmanage : MonoBehaviour
 				 */
 				else if (scene == "breakfast") {
 						if (state == 0 && path == 0) {
-								dialogue = "Climbing down the stairs, you find your way to\n the kitchen, make some food, and shovel\n some of it into your mouth. What did you have?";
+								dialogue = "Climbing down the stairs, you find your way to the kitchen, make some food, and shovel some of it into your mouth.";
 						}
 						if (state == 1 && path == 0) {
-								dialogue = "";
-								if (GUI.Button (new Rect (100, Screen.height - 80, Screen.width - 200, 25), "Eggs and Bacon")) {
+								choosingOption = true;
+								dialogue = "What do you eat? ";
+								
+								if (GUI.Button (new Rect (100, Screen.height - 140, Screen.width - 200, 25), "Eggs and Bacon")) {
 										clickedbutton = 1;
 								}
-								if (GUI.Button (new Rect (100, Screen.height - 50, Screen.width - 200, 25), "Cereal")) {
+								if (GUI.Button (new Rect (100, Screen.height - 110, Screen.width - 200, 25), "Cereal")) {
 										clickedbutton = 2;
 								}
+								if (GUI.Button (new Rect (100, Screen.height - 80, Screen.width - 200, 25), "Leftovers from last night")) {
+										clickedbutton = 3;
+								}
+								if (GUI.Button (new Rect (100, Screen.height - 50, Screen.width - 200, 25), "Mysterious Oozer")) {
+										clickedbutton = 4;
+								}
+								
 						}
+						if (state == 0 && (path == 1 || path == 2)) {
+								dialogue = "Sounds good!";
+						}
+						if (state == 0 && path == 3) {
+								dialogue = "Okay, that's cool; dinner for breakfast is great!";			
+						}
+						if (state == 0 && path == 4) {
+								dialogue = "Whatever floats your boat!";
+						}
+				} else if (scene == "school") {
+							
 				}
 		}
 
