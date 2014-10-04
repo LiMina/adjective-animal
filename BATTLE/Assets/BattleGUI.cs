b5f07a9e2aca8f9cc7a9c28e5abc43f120ae0ffd
Mina Li <li.mina888@berkeley.edu>
2014-10-03 18:51:31 -0700

    Dynamically resizing GUI.

--- BATTLE/Assets/BattleGUI.cs
+++ BATTLE/Assets/BattleGUI.cs
@@ -19,12 +19,12 @@ public class BattleGUI : MonoBehaviour {
 			}
 
 		//HP/MP display
-			GUI.Box (new Rect (50, Screen.height - 200, 300, 150), ""); 
-			GUI.Label (new Rect(75, Screen.height - 180, 300, 25), "HP :" + TurnStateMachine.playerHP);
-			GUI.Label (new Rect (75, Screen.height - 165, 300, 25), "MP :" + TurnStateMachine.playerMP);
+			GUI.Box (new Rect (50, Screen.height - 200, (Screen.width - (Screen.width/8) - 100)/3, 150), ""); 
+			GUI.Label (new Rect(70, Screen.height - 180, (Screen.width - (Screen.width/8) - 100)/3-40, 25), "HP :" + TurnStateMachine.playerHP);
+			GUI.Label (new Rect (70, Screen.height - 165, (Screen.width - (Screen.width/8) - 100)/3-40, 25), "MP :" + TurnStateMachine.playerMP);
 	
 		//Handles whether or not the buttons are locked (due to currently choosing a target)
-			GUI.Box (new Rect (375, Screen.height - 200, 300, 150), "");
+			GUI.Box (new Rect (50+(Screen.width - (Screen.width/8)- 100)/3 + Screen.width/16, Screen.height - 200, (Screen.width - (Screen.width/8)- 100)/3, 150), "");
 			if (TurnStateMachine.getTurn () == 0 && !TurnStateMachine.isCommandTargeting()) {
 				attackLock = false;
 			} 
@@ -35,11 +35,11 @@ public class BattleGUI : MonoBehaviour {
 		//Handles the menu buttons
 			if (TurnStateMachine.commandSelection == TurnStateMachine.SELECT_NONE
 	    			|| TurnStateMachine.commandSelection == TurnStateMachine.SELECT_TARGET_ATTACK) {		//If we're not choosing an ability, show the usual buttons.
-				if (GUI.Button (new Rect (380, Screen.height - 180, 295, 25), "Tackle")) {
+					if (GUI.Button (new Rect (50+(Screen.width - (Screen.width/8) - 100)/3 + Screen.width/16 + 20, Screen.height - 180, (Screen.width - (Screen.width/8) - 100)/3 - 40, 25), "Tackle")) {
 					Debug.Log ("tackle!");
 					TurnStateMachine.commandSelection = TurnStateMachine.SELECT_TARGET_ATTACK;
 				}
-				if (GUI.Button (new Rect (380, Screen.height - 150, 295, 25), "Abilities")) {
+					if (GUI.Button (new Rect (50+(Screen.width - (Screen.width/8)- 100)/3 + Screen.width/16 + 20, Screen.height - 150, (Screen.width - (Screen.width/8) - 100)/3 - 40, 25), "Abilities")) {
 					Debug.Log ("abilities?");
 					TurnStateMachine.commandSelection = TurnStateMachine.SELECT_ABILITY;
 				}
@@ -47,7 +47,7 @@ public class BattleGUI : MonoBehaviour {
 				if (TurnStateMachine.playerMP < 5) {
 					GUI.enabled = false;
 				}
-				if (GUI.Button (new Rect (380, Screen.height - 180, 295, 25), "DIEDIEDIE")) {
+					if (GUI.Button (new Rect (50+(Screen.width - (Screen.width/8)- 100)/3 + Screen.width/16 + 20, Screen.height - 180, (Screen.width - (Screen.width/8)- 100)/3 - 40, 25), "DIEDIEDIE")) {
 					TurnStateMachine.commandSelection = TurnStateMachine.SELECT_TARGET_DIE;
 				}
 				GUI.enabled = !attackLock;
@@ -55,15 +55,15 @@ public class BattleGUI : MonoBehaviour {
 				if (TurnStateMachine.playerMP < 10) {
 					GUI.enabled = false;
 				}
-				if (GUI.Button (new Rect (380, Screen.height - 150, 295, 25), "SPLOSIONS")) {
+					if (GUI.Button (new Rect (50+(Screen.width - (Screen.width/8)- 100)/3 + Screen.width/16 + 20, Screen.height - 150, (Screen.width - (Screen.width/8)- 100)/3 - 40, 25), "SPLOSIONS")) {
 					TurnStateMachine.castSPLOSIONS();
 				}
 				GUI.enabled = !attackLock;
 
-				if (GUI.Button (new Rect (380, Screen.height - 90, 295, 25), "Back")) {
+					if (GUI.Button (new Rect (50+(Screen.width - (Screen.width/8)- 100)/3 + Screen.width/16 + 20, Screen.height - 90, (Screen.width - (Screen.width/8)- 100)/3 - 40, 25), "Back")) {
 					TurnStateMachine.commandSelection = TurnStateMachine.SELECT_NONE;
 				}
 			}
-
+			GUI.Box (new Rect (50+2*(Screen.width - (Screen.width/8)- 100)/3 + Screen.width/8, Screen.height - 200, (Screen.width - (Screen.width/8)- 100)/3, 150), "");
 	}
 }
