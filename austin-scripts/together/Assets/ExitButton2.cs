b7fba01e047dd16dd8cc2d3de7d150d37950df25
austinshyu <austinshyu@berkeley.edu>
2014-10-25 01:56:46 -0700

    exitbutton2

---
+++ austin-scripts/together/Assets/ExitButton2.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitButton2 : MonoBehaviour {
+	public bool clicked = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transitions.currBattle != "wakeup"){
+			clicked = false;
+		}
+	}
+	void OnMouseDown(){
+		clicked = true;
+	}
+	void OnMouseEnter(){
+		this.gameObject.GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("exitbutton_hl");
+	}
+	void OnMouseExit(){
+		this.gameObject.GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("exitbutton");
+	}
+}
